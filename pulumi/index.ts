38fadbe186553b34de28a32e46438d0c1943145d
Josh Kodroff <josh@pulumi.com>
2023-09-13 20:46:05 -0400

    initial commit

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -1,5 +1,5 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";
+import * as azure_native from "@pulumi/azure-native";
 import * as synced_folder from "@pulumi/synced-folder";
 
 // Import the program's configuration settings.
@@ -8,93 +8,70 @@ const path = config.get("path") || "./www";
 const indexDocument = config.get("indexDocument") || "index.html";
 const errorDocument = config.get("errorDocument") || "error.html";
 
-// For testing how these work:
-export const myConfigValue = config.require("my-config-value");
+// Create a resource group for the website.
+const resourceGroup = new azure_native.resources.ResourceGroup("resource-group", {});
 
-
-// Create an S3 bucket and configure it as a website.
-const bucket = new aws.s3.Bucket("bucket", {
-    website: {
-        indexDocument: indexDocument,
-        errorDocument: errorDocument,
+// Create a blob storage account.
+const account = new azure_native.storage.StorageAccount("account", {
+    resourceGroupName: resourceGroup.name,
+    kind: "StorageV2",
+    sku: {
+        name: "Standard_LRS",
     },
 });
 
-// Configure ownership controls for the new S3 bucket
-const ownershipControls = new aws.s3.BucketOwnershipControls("ownership-controls", {
-    bucket: bucket.bucket,
-    rule: {
-        objectOwnership: "ObjectWriter",
-    },
-});
-
-// Configure public ACL block on the new S3 bucket
-const publicAccessBlock = new aws.s3.BucketPublicAccessBlock("public-access-block", {
-    bucket: bucket.bucket,
-    blockPublicAcls: false,
+// Configure the storage account as a website.
+const website = new azure_native.storage.StorageAccountStaticWebsite("website", {
+    resourceGroupName: resourceGroup.name,
+    accountName: account.name,
+    indexDocument: indexDocument,
+    error404Document: errorDocument,
 });
 
 // Use a synced folder to manage the files of the website.
-const bucketFolder = new synced_folder.S3BucketFolder("bucket-folder", {
+const syncedFolder = new synced_folder.AzureBlobFolder("synced-folder", {
     path: path,
-    bucketName: bucket.bucket,
-    acl: "public-read",
-}, { dependsOn: [ownershipControls, publicAccessBlock] });
+    resourceGroupName: resourceGroup.name,
+    storageAccountName: account.name,
+    containerName: website.containerName,
+});
 
-// // Create a CloudFront CDN to distribute and cache the website.
-// const cdn = new aws.cloudfront.Distribution("cdn", {
-//     enabled: true,
-//     origins: [{
-//         originId: bucket.arn,
-//         domainName: bucket.websiteEndpoint,
-//         customOriginConfig: {
-//             originProtocolPolicy: "http-only",
-//             httpPort: 80,
-//             httpsPort: 443,
-//             originSslProtocols: ["TLSv1.2"],
-//         },
-//     }],
-//     defaultCacheBehavior: {
-//         targetOriginId: bucket.arn,
-//         viewerProtocolPolicy: "redirect-to-https",
-//         allowedMethods: [
-//             "GET",
-//             "HEAD",
-//             "OPTIONS",
-//         ],
-//         cachedMethods: [
-//             "GET",
-//             "HEAD",
-//             "OPTIONS",
-//         ],
-//         defaultTtl: 600,
-//         maxTtl: 600,
-//         minTtl: 600,
-//         forwardedValues: {
-//             queryString: true,
-//             cookies: {
-//                 forward: "all",
-//             },
-//         },
-//     },
-//     priceClass: "PriceClass_100",
-//     customErrorResponses: [{
-//         errorCode: 404,
-//         responseCode: 404,
-//         responsePagePath: `/${errorDocument}`,
-//     }],
-//     restrictions: {
-//         geoRestriction: {
-//             restrictionType: "none",
-//         },
-//     },
-//     viewerCertificate: {
-//         cloudfrontDefaultCertificate: true,
-//     },
-// });
+// Create a CDN profile.
+const profile = new azure_native.cdn.Profile("profile", {
+    resourceGroupName: resourceGroup.name,
+    sku: {
+        name: "Standard_Microsoft",
+    },
+});
 
-// Export the URLs and hostnames of the bucket and distribution.
-export const originURL = pulumi.interpolate`http://${bucket.websiteEndpoint}`;
-export const originHostname = bucket.websiteEndpoint;
-// export const cdnURL = pulumi.interpolate`https://${cdn.domainName}`;
-// export const cdnHostname = cdn.domainName;
+// Pull the hostname out of the storage-account endpoint.
+const originHostname = account.primaryEndpoints.apply(endpoints => new URL(endpoints.web)).hostname;
+
+// Create a CDN endpoint to distribute and cache the website.
+const endpoint = new azure_native.cdn.Endpoint("endpoint", {
+    resourceGroupName: resourceGroup.name,
+    profileName: profile.name,
+    isHttpAllowed: false,
+    isHttpsAllowed: true,
+    isCompressionEnabled: true,
+    contentTypesToCompress: [
+        "text/html",
+        "text/css",
+        "application/javascript",
+        "application/json",
+        "image/svg+xml",
+        "font/woff",
+        "font/woff2",
+    ],
+    originHostHeader: originHostname,
+    origins: [{
+        name: account.name,
+        hostName: originHostname,
+    }],
+});
+
+// Export the URLs and hostnames of the storage account and CDN.
+export const originURL = account.primaryEndpoints.apply(endpoints => endpoints.web);
+export { originHostname };
+export const cdnURL = pulumi.interpolate`https://${endpoint.hostName}`;
+export const cdnHostname = endpoint.hostName;
